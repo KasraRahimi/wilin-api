191e4ae8029f2920e542ff05a21ef521db2d2307
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-04-15 22:23:43 -0400

    feat: added signup route

--- src/server/routes/authentication.go
+++ src/server/routes/authentication.go
@@ -1,16 +1,25 @@
 package routes
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
 	"time"
 	"wilin/src/database"
+	"wilin/src/database/roles"
 	"wilin/src/server/utils"
 )
 
 type LoginFields struct {
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
+
+type SignUpFields struct {
+	Email    string `json:"email" form:"email"`
 	Username string `json:"username" form:"username"`
 	Password string `json:"password" form:"password"`
 }
@@ -63,6 +72,59 @@ func (s *Server) HandleLogin(ctx *gin.Context) {
 	}
 
 	ctx.String(http.StatusUnauthorized, "Invalid login information")
+}
+
+func (s *Server) HandleSignup(ctx *gin.Context) {
+	var signUpFields SignUpFields
+	if err := ctx.ShouldBind(&signUpFields); err != nil {
+		ctx.Error(err)
+		ctx.String(http.StatusBadRequest, "Incorrectly formatted")
+		return
+	}
+
+	userDTo := UserDTO{
+		Id:       0,
+		Email:    signUpFields.Email,
+		Username: signUpFields.Username,
+		Password: signUpFields.Password,
+		Role:     roles.USER,
+	}
+
+	if !utils.IsValidEmail(userDTo.Email) {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid email"})
+		return
+	}
+
+	userModel, err := userDTo.ToUserModel()
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = s.UserDao.ReadUserByEmail(userModel.Email)
+	if !errors.Is(err, sql.ErrNoRows) {
+		ctx.JSON(http.StatusConflict, gin.H{"error": "email already taken"})
+		return
+	}
+
+	_, err = s.UserDao.ReadUserByUsername(userModel.Username)
+	if !errors.Is(err, sql.ErrNoRows) {
+		ctx.JSON(http.StatusConflict, gin.H{"error": "username already taken"})
+		return
+	}
+
+	userId, err := s.UserDao.CreateUser(userModel)
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	userDTo.Password = ""
+	userDTo.Id = int(userId)
+
+	ctx.JSON(http.StatusCreated, userDTo)
 }
 
 func (s *Server) Authentication() gin.HandlerFunc {
