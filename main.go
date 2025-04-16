00c461874e64795e0562e20515e376637ccbdfdb
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-04-15 21:20:26 -0400

    feat: added env file and env file loading

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"wilin/src/server"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v\n", err)
+	}
+
 	router := server.New()
-	err := router.Run("0.0.0.0:8080")
+	err = router.Run("0.0.0.0:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
