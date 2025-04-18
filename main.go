9e25458a6ec18d07c21042c781ae9227d7775d24
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-04-18 13:09:25 -0400

    feat: added dictionary and user table creation on startup

--- main.go
+++ main.go
@@ -18,6 +18,16 @@ func main() {
 		log.Fatalf("Error connecting to database: %v\n", err)
 	}
 
+	err = database.CreateDictionaryTable(db)
+	if err != nil {
+		log.Fatalf("Error creating dictionary table: %v\n", err)
+	}
+
+	err = database.CreateUserTable(db)
+	if err != nil {
+		log.Fatalf("Error creating user table: %v\n", err)
+	}
+
 	err = db.Ping()
 	if err != nil {
 		log.Fatalf("Error pinging database: %v\n", err)
