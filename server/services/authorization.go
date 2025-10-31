7290d71ceb960436f9d7c63dca718d1c7e58ea30
Kasra Rahimi <kasra012345@gmail.com>
2025-10-31 03:04:51 -0400

    feat: permissions and roles to services

--- server/services/authorization.go
+++ server/services/authorization.go
@@ -20,3 +20,71 @@ func (r Role) String() string {
 		return ""
 	}
 }
+
+func (r Role) Can(p Permission) bool {
+	return p.IsPermissionInArray(permissionsMap[r])
+}
+
+func NewRole(role string) Role {
+	switch role {
+	case ROLE_NON_USER.String():
+		return ROLE_NON_USER
+	case ROLE_USER.String():
+		return ROLE_USER
+	case ROLE_ADMIN.String():
+		return ROLE_ADMIN
+	default:
+		return ROLE_NON_USER
+	}
+}
+
+type Permission int
+
+const (
+	PERMISSION_VIEW_WORD Permission = iota
+	PERMISSION_ADD_WORD
+	PERMISSION_DELETE_WORD
+	PERMISSION_MODIFY_WORD
+	PERMISSION_ADD_PROPOSAL
+	PERMISSION_VIEW_ALL_PROPOSAL
+	PERMISSION_VIEW_SELF_PROPOSAL
+	PERMISSION_MODIFY_ALL_PROPOSAL
+	PERMISSION_MODIFY_SELF_PROPOSAL
+	PERMISSION_DELETE_ALL_PROPOSAL
+	PERMISSION_DELETE_SELF_PROPOSAL
+)
+
+var permissionsMap = map[Role][]Permission{
+	ROLE_ADMIN: {
+		PERMISSION_VIEW_WORD,
+		PERMISSION_ADD_WORD,
+		PERMISSION_DELETE_WORD,
+		PERMISSION_MODIFY_WORD,
+		PERMISSION_ADD_PROPOSAL,
+		PERMISSION_VIEW_ALL_PROPOSAL,
+		PERMISSION_VIEW_SELF_PROPOSAL,
+		PERMISSION_MODIFY_ALL_PROPOSAL,
+		PERMISSION_MODIFY_SELF_PROPOSAL,
+		PERMISSION_DELETE_ALL_PROPOSAL,
+		PERMISSION_DELETE_SELF_PROPOSAL,
+	},
+	ROLE_USER: {
+		PERMISSION_VIEW_WORD,
+		PERMISSION_ADD_PROPOSAL,
+		PERMISSION_VIEW_SELF_PROPOSAL,
+		PERMISSION_MODIFY_SELF_PROPOSAL,
+		PERMISSION_DELETE_SELF_PROPOSAL,
+	},
+	ROLE_NON_USER: {
+		PERMISSION_VIEW_WORD,
+	},
+}
+
+func (p Permission) IsPermissionInArray(permissionArray []Permission) bool {
+	for _, permissionValue := range permissionArray {
+		if p == permissionValue {
+			return true
+		}
+	}
+	return false
+}
