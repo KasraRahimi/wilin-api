40ac825455439e5b8a72f318f917ab4920b379e6
Kasra Rahimi <kasra012345@gmail.com>
2025-10-31 02:46:59 -0400

    feat: created authorization related roles

--- server/services/authorization.go
+++ server/services/authorization.go
@@ -1,1 +1,22 @@
 package services
+
+type Role int
+
+const (
+	NonUser Role = iota
+	User
+	Admin
+)
+
+func (r Role) String() string {
+	switch r {
+	case NonUser:
+		return ""
+	case User:
+		return "user"
+	case Admin:
+		return "admin"
+	default:
+		return ""
+	}
+}
