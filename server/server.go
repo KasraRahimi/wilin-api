7518ac7ae1eb5871b41d2e2b947d44341f8b2f3a
Kasra Rahimi <kasra012345@gmail.com>
2025-10-31 02:49:09 -0400

    feat: added sign up route to server

--- server/server.go
+++ server/server.go
@@ -39,12 +39,15 @@ func New(db *sql.DB) *echo.Echo {
 
 	// add routes
 	server.GET("/", router.HelloWorld)
+
 	server.GET("/kalan", router.GetAllKalan)
 	server.GET("/kalan/paginated", router.GetKalanBySearch)
 	server.GET("/kalan/:id", router.GetKalanByID)
 	server.POST("/kalan", router.AddKalan)
 	server.PUT("/kalan", router.UpdateKalan)
 	server.DELETE("/kalan/:id", router.DeleteKalan)
+
+	server.POST("/signup", router.HandleSignUp)
 
 	return server
 }
