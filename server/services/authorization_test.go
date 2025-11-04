44393763f250e8c74b9006c26ec726f1d96ca041
Kasra Rahimi <kasra012345@gmail.com>
2025-11-04 00:35:16 -0500

    feat: added tests for role strings and can

---
+++ server/services/authorization_test.go
@@ -0,0 +1,62 @@
+package services_test
+
+import (
+	"strings"
+	"testing"
+
+	"wilin.info/api/server/services"
+)
+
+func failTest[T any](t *testing.T, got T, want T) {
+	t.Errorf("got: %v, want: %v\n", got, want)
+}
+
+type RoleStringValue struct {
+	role     services.Role
+	expected string
+}
+
+var RoleStringValues = []RoleStringValue{
+	{services.ROLE_NON_USER, ""},
+	{services.ROLE_USER, "user"},
+	{services.ROLE_ADMIN, "admin"},
+}
+
+func TestRoleString(t *testing.T) {
+	for _, testValue := range RoleStringValues {
+		roleString := testValue.role.String()
+		if strings.Compare(roleString, testValue.expected) != 0 {
+			failTest(t, roleString, testValue.expected)
+		}
+	}
+}
+
+type RoleCanValue struct {
+	role       services.Role
+	permission services.Permission
+	expected   bool
+}
+
+var RoleCanValues = []RoleCanValue{
+	{services.ROLE_NON_USER, services.PERMISSION_VIEW_WORD, true},
+	{services.ROLE_NON_USER, services.PERMISSION_MODIFY_WORD, false},
+	{services.ROLE_NON_USER, services.PERMISSION_DELETE_WORD, false},
+	{services.ROLE_NON_USER, services.PERMISSION_VIEW_ALL_PROPOSAL, false},
+	{services.ROLE_USER, services.PERMISSION_VIEW_SELF_PROPOSAL, true},
+	{services.ROLE_USER, services.PERMISSION_VIEW_ALL_PROPOSAL, false},
+	{services.ROLE_USER, services.PERMISSION_DELETE_WORD, false},
+	{services.ROLE_USER, services.PERMISSION_MODIFY_SELF_PROPOSAL, true},
+	{services.ROLE_ADMIN, services.PERMISSION_DELETE_ALL_PROPOSAL, true},
+	{services.ROLE_ADMIN, services.PERMISSION_DELETE_WORD, true},
+	{services.ROLE_ADMIN, services.PERMISSION_MODIFY_WORD, true},
+	{services.ROLE_ADMIN, services.PERMISSION_VIEW_ALL_PROPOSAL, true},
+}
+
+func TestRoleCan(t *testing.T) {
+	for _, testValue := range RoleCanValues {
+		output := testValue.role.Can(testValue.permission)
+		if output != testValue.expected {
+			failTest(t, output, testValue.expected)
+		}
+	}
+}
