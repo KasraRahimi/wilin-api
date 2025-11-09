40cee63451372dade4bc6cdf4c9d29466b0507fa
Kasra Rahimi <kasra012345@gmail.com>
2025-11-09 17:03:42 -0500

    feat: added new recovery table for password changing

---
+++ sqlc/recovery/queries.sql
@@ -0,0 +1,11 @@
+-- name: Create :execresult
+INSERT INTO recoveries (id, user_id) VALUES (?, ?);
+
+-- name: ReadByID :one
+SELECT * FROM recoveries WHERE id = ? LIMIT 1;
+
+-- name: ReadByUserID :many
+SELECT * FROM recoveries where user_id = ?;
+
+-- name: DeleteByID :execresult
+DELETE FROM recoveries WHERE id = ?;
---
+++ sqlc/recovery/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS recoveries (
+    id varchar(255) PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
